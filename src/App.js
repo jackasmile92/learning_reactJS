64e9f5c2c4de1aeb00516478bf166c90f5c35433
Ekaterina Antonova <antonova.ekaterina@accenture.com>
2020-09-07 22:37:57 +0300

    lesson 19 + homework

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar.jsx'
 import Profile from './components/Profile/Profile.jsx';
 import Dialogs from './components/Dialogs/Dialogs.jsx';
+import { Route, BrowserRouter } from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className='app-wrapper-content'>
-        {/*<Profile/>*/}
-        <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/profile"component={Profile} />
+          <Route path="/news"component={News} />
+          <Route path="/music"component={Music} />
+          <Route path="/settings"component={Settings} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 
 }

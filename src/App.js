2cae20575b7354508e36e9c6410e774f4494fe85
Ekaterina Antonova <antonova.ekaterina@accenture.com>
2020-09-07 14:52:30 +0300

    lesson 18

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar.jsx'
 import Profile from './components/Profile/Profile.jsx';
+import Dialogs from './components/Dialogs/Dialogs.jsx';
 
 const App = () => {
-return(
-    <div className= 'app-wrapper'>
-      <Header/>
-      <Navbar/> 
-      <Profile/>
-  </div>
-);
+  return (
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <div className='app-wrapper-content'>
+        {/*<Profile/>*/}
+        <Dialogs />
+      </div>
+    </div>
+  );
 
 }
 

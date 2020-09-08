60b23b0609185f9d829498354b9afb15363b7bdc
Ekaterina Antonova <antonova.ekaterina@accenture.com>
2020-09-08 10:00:56 +0300

    lesson 21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import s from './Dialogs.module.css';
 
-const Dialogs = (props) =>{
-    return(
-        <div>
-            Dialogs
+const Dialogs = (props) => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog+' '+s.active}>
+                    Name 1
+                </div>
+                <div className={s.dialog}>
+                    Name 2
+                </div>
+                <div className={s.dialog}>
+                    Name 3
+                </div>
+                <div className={s.dialog}>
+                    Name 4
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>how are you?</div>
+                <div className={s.message}>Yooooo</div>
+            </div>
         </div>
     );
 }

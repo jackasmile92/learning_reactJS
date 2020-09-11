4ecfe9969aad8197dd556f2bde01f943965d3763
Ekaterina Antonova <antonova.ekaterina@accenture.com>
2020-09-11 17:53:01 +0300

    lesson 31+ homework

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,7 +6,7 @@ import Message from './Message/Message.jsx';
 
 
 const Dialogs = (props) => {
-    let dialogs =props.state.dialogs;
+    let dialogs = props.state.dialogs;
 
     let messagesData = props.state.messagesData;
 
@@ -16,6 +16,12 @@ const Dialogs = (props) => {
     let messages = messagesData
         .map(m => <Message message={m.message} />);
 
+    let newMessageElement = React.createRef();
+    let addMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    };
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -23,6 +29,12 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messages}
+                <div>
+                    <textarea ref={newMessageElement}></textarea>
+                </div>
+                <div>
+                    <button onClick={addMessage}>Add message</button>
+                </div>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,17 +6,23 @@ const MyPosts = (props) => {
 
   let posts = props.posts;
 
-let postsElements = posts.map(p => <Post message={p.message} likeCount={p.likeCount} />)
+  let postsElements = posts.map(p => <Post message={p.message} likeCount={p.likeCount} />)
+
+  let newPostElement = React.createRef();
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
 
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
       </div>
       <div className={s.posts}>

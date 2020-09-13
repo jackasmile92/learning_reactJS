f4bd4740b3929b42ebe90bb5f613e16df1a6d2c9
Ekaterina Antonova <antonova.ekaterina@accenture.com>
2020-09-13 21:26:54 +0300

    lesson 37

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, { subscribe } from "./redux/state.js"
+import store from "./redux/state.js"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { addPost, updateNewPostText } from './redux/state.js';
 
 export let rerenderEntireTree = (state) => {
 
   ReactDOM.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+      <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
     </React.StrictMode>,
     document.getElementById('root')
   );
@@ -18,9 +17,9 @@ export let rerenderEntireTree = (state) => {
 
 
 
-rerenderEntireTree(state); 
+rerenderEntireTree(store.getState()); 
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,70 +1,72 @@
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hi how are you', likeCount: '12' },
+                { id: 2, message: 'its my first post', likeCount: '11' },
+                { id: 3, message: 'Hi how are you', likeCount: '13' },
+                { id: 4, message: 'its my first post', likeCount: '141' }
+            ],
+            newPostText: 'newtext'
+        },
+        dialogsPage: {
+            messagesData: [
+                { id: 1, message: "Hi" },
+                { id: 2, message: "how are you?" },
+                { id: 3, message: "Yooooo" }
+            ],
+            dialogs: [
+                { id: 1, name: 'Name 1' },
+                { id: 2, name: 'Name 2' },
+                { id: 3, name: 'Name 3' },
+                { id: 4, name: 'Name 4' }
+            ]
+        },
+        siteBar: {
+            friends: [
+                {
+                    name: 'Васек Писек',
+                    avatar: 'https://i.redd.it/v0caqchbtn741.jpg'
+                },
+                {
+                    name: 'Алена Пелена',
+                    avatar: 'https://telegram.org/file/464001801/4/pPObBDJVv-M.32191.png/9963667389a3218249'
+                },
+                {
+                    name: 'Максим Угостим',
+                    avatar: 'https://wl-brightside.cf.tsp.li/resize/728x/jpg/91b/430/964a9c5ac9933cc012d0bd80be.jpg'
+                }
 
-let rerenderEntireTree = () =>{
-    console.log('state was changed');
-}
+            ]
+        }
 
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Hi how are you', likeCount: '12' },
-            { id: 2, message: 'its my first post', likeCount: '11' },
-            { id: 3, message: 'Hi how are you', likeCount: '13' },
-            { id: 4, message: 'its my first post', likeCount: '141' }
-        ],
-        newPostText : 'newtext'
     },
-    dialogsPage: {
-        messagesData: [
-            { id: 1, message: "Hi" },
-            { id: 2, message: "how are you?" },
-            { id: 3, message: "Yooooo" }
-        ],
-        dialogs: [
-            { id: 1, name: 'Name 1' },
-            { id: 2, name: 'Name 2' },
-            { id: 3, name: 'Name 3' },
-            { id: 4, name: 'Name 4' }
-        ]
+    getState(){
+        return this._state;
     },
-    siteBar:{
-        friends: [
-            {
-                name: 'Васек Писек', 
-                avatar: 'https://i.redd.it/v0caqchbtn741.jpg'
-            },
-            {
-                name: 'Алена Пелена', 
-                avatar: 'https://telegram.org/file/464001801/4/pPObBDJVv-M.32191.png/9963667389a3218249'
-            },
-            {
-                name: 'Максим Угостим',
-                avatar:'https://wl-brightside.cf.tsp.li/resize/728x/jpg/91b/430/964a9c5ac9933cc012d0bd80be.jpg'
-            }
-            
-        ]
+    _callSubscriber() {
+        console.log('state was changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likeCount: 0
+        };
+
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer){
+        this._callSubscriber = observer;
     }
-
-};
-
-export const addPost =() => {
-    let newPost ={
-        id: 5,
-        message: state.profilePage.newPostText,
-        likeCount: 0
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
 }
 
-export const updateNewPostText =(newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
 
-export const subscribe = (observer) =>{
-    rerenderEntireTree = observer;
-} 
-
-export default state;
+export default store;
+window.store = store;

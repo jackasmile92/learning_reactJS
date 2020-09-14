1ad21afd16644a29a82193d3378cad671852516c
Ekaterina Antonova <antonova.ekaterina@accenture.com>
2020-09-14 17:10:21 +0300

    lesson 39

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import { updateNewPostTextActionCreator, addPostActionCreator } from '../../../redux/state';
+
 
 const MyPosts = (props) => {
 
@@ -12,13 +14,12 @@ const MyPosts = (props) => {
 
 
   let addPost = () => {
-    props.dispatch({type:'ADD-POST'});
-    props.dispatch({type:'UPDATE-NEW-POST-TEXT', text: ''});
+    props.dispatch(addPostActionCreator());
   };
 
-  let onPostChange = () =>{
+  let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.dispatch({type:'UPDATE-NEW-POST-TEXT', text: text});
+    props.dispatch(updateNewPostTextActionCreator(text) );
   };
 
 
@@ -27,7 +28,7 @@ const MyPosts = (props) => {
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
+          <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
         </div>
         <div>
           <button onClick={addPost}>Add post</button>

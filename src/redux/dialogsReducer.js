ff87863ba783ba48e68da8ce3f5c811c204cda09
Ekaterina Antonova <antonova.ekaterina@accenture.com>
2020-09-21 22:38:38 +0300

    lesson 47

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -17,17 +17,20 @@ let initialState ={
 
 const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_MESSAGE:
+        case ADD_MESSAGE:{
             let newMessage = {
                 id: 5,
                 message: state.newDialogText
             };
-            state.messagesData.push(newMessage);
-            state.newDialogText = '';
-            return state;
-        case UPDATE_NEW_MESSAGE:
-            state.newDialogText = action.text;
-            return state;
+            let stateCopy ={...state};
+            stateCopy.messagesData =[...state.messagesData];
+            stateCopy.messagesData.push(newMessage);
+            stateCopy.newDialogText = '';
+            return stateCopy;}
+        case UPDATE_NEW_MESSAGE:{
+            let stateCopy ={...state};
+            stateCopy.newDialogText = action.text;
+            return stateCopy;}
         default: return state;
     }
 
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -13,18 +13,21 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST:{
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likeCount: 0
             };
-            state.posts.push(newPost);
-            state.newPostText = '';
-            return state;
-        case UPDATE_NEW_POST_ITEM:
-            state.newPostText = action.text;
-            return state;
+            let stateCopy ={...state};
+            stateCopy.posts = [...state.posts];
+            stateCopy.posts.push(newPost);
+            stateCopy.newPostText = '';
+            return stateCopy;}
+        case UPDATE_NEW_POST_ITEM:{
+            let stateCopy ={...state};
+            stateCopy.newPostText = action.text;
+            return stateCopy;}
         default: return state;
     }
 }
